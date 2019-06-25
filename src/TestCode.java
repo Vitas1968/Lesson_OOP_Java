a05e3592f88347ef70d4e3e5de2552173bcdd9ba
Vitas1968 <dor1968@mail.ru>
2019-06-25 17:46:02 +0700

    Четвертое задание в файле TestCode

--- src/TestCode.java
+++ src/TestCode.java
@@ -1,5 +1,24 @@
-package PACKAGE_NAME;
-
 public class TestCode
 {
+    public static void main(String[] args)
+    {
+        // Задание 4 Создать массив из 5 сотрудников,
+        // С помощью цикла вывести информацию только о сотрудниках старше 40 лет
+        Employee [] employees = new Employee [5];
+
+        employees[0]=new Employee("Раздолбаев","Директор", "ra@mail.com", "89012345678", 90000,47);
+        employees[1]=new Employee("Криворуков","Программист", "pro@mail.com", "89012342346", 70000,27);
+        employees[2]=new Employee("Сидоров","Инженер","ing@mail.com", "89014325678", 50000,36);
+        employees[3]=new Employee("Мешалкина","Менеджер","manager@mail.com", "86154325678", 30000,24);
+        employees[4]=new Employee("Грязнова","Уборщица","clean@mail.com", "86154329345", 15000,60);
+
+        for (Employee e: employees)
+        {
+            if (e.getAge() > 40)
+            {
+                e.infoEmployee();
+            }
+        }
+
+    }
 }
